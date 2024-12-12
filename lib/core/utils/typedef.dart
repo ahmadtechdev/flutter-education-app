3d55db28545d62e2fde3d8270513fbd47ebcde69
ahmadtechdev <ahmadtechdev@gmail.com>
2024-12-12 12:00:25 +0500

    some work done

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -5,3 +5,4 @@ typedef ResultFuture<T> = Future<Either<Failure,T>>;
 
 typedef ResultVoid = ResultFuture<void>;
 
+typedef DataMap = Map<String, dynamic>;
---
+++ test/src/authentication/domain/usecases/authentication_repository.mock.dart
@@ -0,0 +1,5 @@
+
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_tutorial/src/authentication/domain/repositories/%20authentication_repository.dart';
+
+class MockAuthRepo extends Mock implements AuthenticationRepository{}
